f227abb18b632b7021668430a280a53e116905e5
Karolina Wolan <wolanka739@gmail.com>
2022-03-24 11:07:10 +0100

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -31,6 +31,13 @@ function App() {
     }))
   };
 
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true,
+    })));
+  };
+
   return (
     <Container>
       <Header
@@ -56,7 +63,9 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
-          />}
+            setAllDone={setAllDone}
+          />
+        }
       />
     </Container>
   );
